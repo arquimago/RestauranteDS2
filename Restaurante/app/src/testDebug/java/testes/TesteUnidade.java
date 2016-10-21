a9f450c0a09d7450f5502fc953cfcbb8991711b8
Thales Francisco <thales@alvesnet.net>
2016-10-20 22:09:05 -0300

    ultimas mudanças

--- Restaurante/app/src/testDebug/java/testes/TesteUnidade.java
+++ Restaurante/app/src/testDebug/java/testes/TesteUnidade.java
@@ -21,47 +21,45 @@ import static junit.framework.Assert.assertEquals;
 
 public class TesteUnidade {
 
-   //public TesteUnidade(){
-      Item item = new Item("Farinha",50,"kg",10);
-      Fornecedor fornecedor = new Fornecedor("Fernando","234-5678","123456789/11","email@email.com");
-      Compra compra = new Compra(item,100,100,"10/10/16",fornecedor);
-      Endereco endereco = new Endereco("logradouro", "rua", 10, "String bairro", "Aracaju", "SE", "49000-000");
-      Garcom garcom = new Garcom("Messias",endereco,"234-5678","123456789-00","messias");
-      Gerente gerente = new Gerente("Melo",endereco,"234-5678","123456789-00","melo");
-      Ingrediente ingrediente = new Ingrediente(item, 2);
-      Mesa mesa = new Mesa(1);
-      /*ArrayList<Ingrediente> ingredientes = new ArrayList();
-      ingredientes.add(ingrediente);
-      Produto produto = new Produto("Pizza",10,ingredientes);
-      ArrayList<Produto> produtos = new ArrayList();
-      produtos.add(produto);
-      Pedido pedido = new Pedido(produtos);*/
-   //}
+   Item item = new Item("Farinha",50,"kg",10);
+   Fornecedor fornecedor = new Fornecedor("Fernando","234-5678","123456789/11","email@email.com");
+   Compra compra = new Compra(item,100,100,"10/10/16",fornecedor);
+   Endereco endereco = new Endereco("logradouro", "rua", 10, "String bairro", "Aracaju", "SE", "49000-000");
+   Garcom garcom = new Garcom("Messias",endereco,"234-5678","123456789-00","messias");
+   Gerente gerente = new Gerente("Melo",endereco,"234-5678","123456789-00","melo");
+   Ingrediente ingrediente = new Ingrediente(item, 2);
+   Mesa mesa = new Mesa(1);
+   ArrayList<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
+   ingredientes.add(ingrediente);
+   Produto produto = new Produto("Pizza",10,ingredientes);
+   ArrayList <Produto> produtos = new  ArrayList <Produto>();
+   produtos.add(produto);
+   Pedido pedido = new Pedido(produtos);
 
    /* vamos testaresssas classes acima e ver no que dá*/
    @Test
-   public void TestItemgetNome(){
+   public void TestItemgetNome() throws Exception{
       assertEquals("Farinha",item.getNome());
       item.setNome("Farofa");
       assertEquals("Farofa",item.getNome());
    }
 
    @Test
-   public void TestItemGetQuantidade(){
+   public void TestItemGetQuantidade()throws Exception{
       assertEquals(50,item.getQuantidade());
       item.setQuantidade(200);
       assertEquals(200,item.getQuantidade());
    }
 
    @Test
-   public void TestItemGetLimiteMinimo(){
+   public void TestItemGetLimiteMinimo()throws Exception{
       assertEquals(10,item.getLimiteMinimo());
       item.setLimiteMinimo(5);
       assertEquals(5,item.getLimiteMinimo());
    }
 
    @Test
-   public void TestItemGetUnidade(){
+   public void TestItemGetUnidade()throws Exception{
       assertEquals("kg",item.getUnidade());
       item.setUnidade("litro");
       assertEquals("litro",item.getUnidade());
