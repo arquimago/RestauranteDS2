bdf3e59aeb646751b30ca663330cb7b5a1e7b7b3
Thales Francisco <thales@alvesnet.net>
2016-10-20 23:38:27 -0300

    corrigindo

--- Restaurante/app/src/testDebug/java/testes/TesteUnidade.java
+++ Restaurante/app/src/testDebug/java/testes/TesteUnidade.java
@@ -23,11 +23,6 @@ import static junit.framework.Assert.assertEquals;
 public class TesteUnidade {
 
    Item item = new Item("Farinha",50,"kg",10);
-   Fornecedor fornecedor = new Fornecedor("Fernando","234-5678","123456789/11","email@email.com");
-   Compra compra = new Compra(item,100,100,"10/10/16",fornecedor);
-   Endereco endereco = new Endereco("logradouro", "rua", 10, "String bairro", "Aracaju", "SE", "49000-000");
-   Garcom garcom = new Garcom("Messias",endereco,"234-5678","123456789-00","messias");
-   Gerente gerente = new Gerente("Melo",endereco,"234-5678","123456789-00","melo");
    Ingrediente ingrediente = new Ingrediente(item, 2);
    Mesa mesa = new Mesa(1);
    ArrayList<Ingrediente> ingredientes = new ArrayList<Ingrediente>();
